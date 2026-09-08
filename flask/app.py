cedb04fc9fb9539b6ce687abf4e0dbc13e2060ad
Taro Fukunaga <tarof429@gmail.com>
2026-09-08 13:44:09 -0700

    Add new pipeline that takes arguments

--- flask/app.py
+++ flask/app.py
@@ -31,7 +31,7 @@ def create_app(mode=None):
 
     @app.route('/test')
     def test():
-        return '<html><body>test</body></html>'
+        return '<html><body>Test Page</body></html>'
 
     @app.errorhandler(404)
     def page_not_found(error):
